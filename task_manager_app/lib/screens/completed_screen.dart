09fd541bfc2f2f9a11872122c00a931ee7f34d86
Asim Naseem <163127884+asimch839@users.noreply.github.com>
2026-04-03 00:27:32 +0500

    Update completed_screen.dart

--- task_manager_app/lib/screens/completed_screen.dart
+++ task_manager_app/lib/screens/completed_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import '../models/task_model.dart';
 import '../providers/task_provider.dart';
 import '../widgets/task_tile.dart';
