4c8ef8d4d2ee3ab5c4cd98d97b72f11460bb2caa
Asim Naseem <163127884+asimch839@users.noreply.github.com>
2026-04-02 23:49:06 +0500

    Update constants.dart

--- ai_chat_app/lib/core/utils/constants.dart
+++ ai_chat_app/lib/core/utils/constants.dart
@@ -2,5 +2,5 @@ class AppConstants {
   static const String appName = 'NovaChat';
   
   // Gemini API Key from Google AI Studio
-  static const String geminiApiKey = 'AIzaSyAjf2EwndjAnWLgPHB_Urnuv_DHsfAaKQc';
+  static const String geminiApiKey = 'AIzaSyACcG1z4lg9Tqb5gP7W4ORYeydSougYqro';
 }
